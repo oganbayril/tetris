c5e4b0145fbd36efb8276defee1502ddb66cc6af
Ogan <oganby@gmail.com>
2025-03-31 21:37:18 +0300

    Update databases.py

--- databases.py
+++ databases.py
@@ -3,12 +3,12 @@ import json
 import pygame
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-options_file = f"{script_dir}\options.json"
 score_file = f"{script_dir}\scores.json"
-resolutions_file = f"{script_dir}\resolutions.json"
+options_file = f"{script_dir}\options.json"
 
-# Default keys for tetris
-default_keys = {
+# Default keys and resolution scaling index
+default_options = {
+    "keys": {
         "MOVE RIGHT": pygame.K_RIGHT,
         "MOVE LEFT": pygame.K_LEFT,
         "ROTATE RIGHT": pygame.K_UP,
@@ -16,16 +16,18 @@ default_keys = {
         "SOFT DROP": pygame.K_DOWN,
         "HARD DROP": pygame.K_SPACE,
         "HOLD": pygame.K_c
+    },
+    "resolution_scale_index": 1  # Default scale index (960x540)
 }
 
-# Getting keys
+# Getting the keys and the resolution
 if os.path.exists(options_file):
     with open(options_file, "r") as file:
-        keys = json.load(file)
+        options = json.load(file)
 else:
-    keys = default_keys
+    options = default_options
     with open(options_file, "w") as file:
-        json.dump(keys, file, indent=4)
+        json.dump(options, file, indent=4)
 
 # Getting high scores
 if os.path.exists(score_file):
